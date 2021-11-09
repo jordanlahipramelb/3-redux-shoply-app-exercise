00f990bb9665340525271279754c60de8ec11b3d
Jordan Lahip-Ramelb <jordan.lahipramelb@gmail.com>
2021-11-08 18:06:14 -0800

    changes

---
+++ src/helpers.js
@@ -0,0 +1,22 @@
+// Calculate the total price rounded to two decimals
+// toFixed returns a string so we convert to a number
+
+export function calculateCartTotal(products, cart) {
+  let total = 0;
+  for (let id in products) {
+    const { price } = products[id];
+    const quantity = cart[id] || 0;
+    total += price * quantity;
+  }
+
+  return total.toFixed(2);
+}
+
+// can't just calculate the length so we sum the quantity key for each item
+export function calculateTotalQuantity(cart) {
+  let totalQuantity = 0;
+  for (let id in cart) {
+    totalQuantity += cart[id];
+  }
+  return totalQuantity;
+}
